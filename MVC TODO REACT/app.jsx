5b08c563f46f40b728d27d55e2d8cd2a9f888f4b
Deysi <sude.90@gmail.com>
2017-10-04 14:37:11 -0500

    check no habilitado

--- MVC TODO REACT/app.jsx
+++ MVC TODO REACT/app.jsx
@@ -1,100 +1,104 @@
 
 class Model {
 
-   constructor () {
-      this.todos = [];
-      this.inputValue = null;
-      this.render = undefined;
+      constructor() {
+            this.todos = [];
+            this.inputValue = null;
+            this.render = undefined;
+            this.listItem = 
+    
    }
   
-   subscribe(render) {
-      this.render = render;
-   }
-   inform() {
-      console.log(this.todos.map(e => e.text));
-      this.render();
-   }
-   addTodo(text) {
-      this.todos.push({
-         id: Utils.uuid(),
-         text: text,
-         completed: false
-      });
-      this.inform();
-   }
-   updateTodo(index, todo) {
-      this.todos[index] = todo;
-      this.inform();
-   }
-   removeTodo(todo) {
-      this.todos = this.todos.filter(item => item !== todo);
-      this.inform();
-   }
-   checkTodo(todo){
-     
-
-   }
+      subscribe(render) {
+            this.render = render;
+      }
+      inform() {
+            console.log(this.todos.map(e => e.text));
+            this.render();
+      }
+      addTodo(text) {
+            this.todos.push({
+                  id: Utils.uuid(),
+                  text: text,
+                  completed: false
+            });
+            this.inform();
+      }
+      updateTodo(index, todo) {
+            this.todos[index] = todo;
+            this.inform();
+      }
+      removeTodo(todo) {
+            this.todos = this.todos.filter(item => item !== todo);
+            this.inform();
+      }
+      checkTodo(todo) {
+            if (checked) {
+                  listItem.className = 'responded';
+            } else {
+                  listItem.className = '';
+            }
+            this.inform();
+      }
 }
 
 
-                       
-      
 
-  
+
 const App = ({ title, model }) => {
-   const items = model.todos.map((todo, index) => {
+      const items = model.todos.map((todo, index) => {
+            return (
+                  <li key={todo.id}>
+                        <input
+                              type="text"
+                              value={todo.text}
+                              onChange={e =>
+                                    model.updateTodo(index, {
+                                          id: todo.id,
+                                          text: e.target.value,
+                                          completed: todo.completed
+                                    })}
+                        />
+                        <label >Confirmed<input className="responded" type="checkbox" ></input></label>
+
+
+                        <button onClick={() => model.removeTodo(todo)}> remove</button>
+                  </li>
+
+            );
+      });
       return (
-         <li key={todo.id}>
-            <input
-               type="text"
-               value={todo.text}
-               onChange={e =>
-                  model.updateTodo(index, {
-                     id: todo.id,
-                     text: e.target.value,
-                     completed: todo.completed
-                  })}
-            />
-            <label >Confirmed<input type="checkbox" ></input></label>
-            
-            
-            <button onClick={() => model.removeTodo(todo)}> remove</button>
-         </li>
-        
+
+            <div>
+                  <header>
+                        <h1> {title} </h1>
+                        <p>Registration App</p>
+                  </header>
+                  <form id="registrar"
+                        onSubmit={e => {
+                              e.preventDefault();
+                              model.addTodo(model.inputValue);
+                        }}
+                  >
+                        <input onChange={e => (model.inputValue = e.target.value)} placeholder="Invite Someone"></input>
+                        <button type="submit">Submit</button>
+
+
+                  </form>
+                  <h2>Invitess</h2>
+                  <ul id="invitedList"> {items} </ul>
+            </div>
       );
-   });
-   return (
-         
-      <div>
-            <header>
-         <h1> {title} </h1>
-         <p>Registration App</p>
-         </header>
-         <form id="registrar" 
-            onSubmit={e => {
-               e.preventDefault();
-               model.addTodo(model.inputValue);
-            }}
-         >
-            <input onChange={e => (model.inputValue = e.target.value)} placeholder="Invite Someone"></input>
-            <button type="submit">Submit</button>
-            
-            
-         </form>
-         <h2>Invitess</h2>
-          <ul id="invitedList"> {items} </ul>
-      </div>
-   );
 };
 
 let model = new Model();
 let counter = 1;
 let render = () => {
-   console.log('render times: ', counter++);
-   ReactDOM.render(
-      <App title="RSVP" model={model} />,
-      document.getElementById('container')
-   );
+      console.log('render times: ', counter++);
+      ReactDOM.render(
+            <App title="RSVP" model={model} />,
+            document.getElementById('container')
+      );
 };
 
 model.subscribe(render);
